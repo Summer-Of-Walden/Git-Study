e5eda25843afad02585ec93ebcc34431b832a8e4
houbowen <1877095981@qq.com>
2025-05-29 14:52:43 +0800

    new file HelloWorld.java

--- src/main/java/com/hit/gitdemo/GitdemoApplication.java
+++ src/main/java/com/hit/gitdemo/GitdemoApplication.java
@@ -3,6 +3,9 @@ package com.hit.gitdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 侯博文
+ */
 @SpringBootApplication
 public class GitdemoApplication {
 
---
+++ src/main/java/com/hit/gitdemo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.hit.gitdemo;
+
+/**
+ * @author 侯博文
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
